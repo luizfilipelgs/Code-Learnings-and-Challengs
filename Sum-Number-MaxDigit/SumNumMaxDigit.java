e120e3afe7f5a3a8cbda1d1e3b8532cbc27bad64
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-12 10:38:00 -0300

    feat: add SumNumMaxDigit - JAVA

--- Sum-Number-MaxDigit/SumNumMaxDigit.java
+++ Sum-Number-MaxDigit/SumNumMaxDigit.java
@@ -1,11 +1,32 @@
-package Decomposição;
-
+import java.util.Random;
 
 public class SumNumMaxDigit {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    public static int main(int maxDigit) {
+        int[] num = new int[4];
+        int sum = 0;
+        Random rand = new Random();
+        
+        for (int i = 0; i < num.length; i++) {
+            int digit = rand.nextInt(maxDigit) + 1;
+            num[i] = digit;
+            sum += digit;
+        }
+        
+        int totalDigit = Integer.parseInt(String.join("", 
+            Integer.toString(num[0]), 
+            Integer.toString(num[1]), 
+            Integer.toString(num[2]), 
+            Integer.toString(num[3])
+        ));
+        
+        if (sum == 21) {
+            return totalDigit;
+        } else {
+            return main(maxDigit);
+        }
+    }
 
-  }
+    public static void main(String[] args) {
+        System.out.println(main(6));
+    }
 }
-
-
