3c5f08966ce6f284acb80ddb673cc53555d9fa03
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-07-03 17:50:22 -0300

    feat: hello Word c++

---
+++ Learnings/C++/Fundamentos/helloword.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    //std::cout << "Hello, World!!!!!!!" << std::endl; ----- Caso não houvesse 'o using namespace std;' na linha 4.
+    cout << "Hello, World!!!!!!!" << endl;
+    return 0;
+}
+
+
