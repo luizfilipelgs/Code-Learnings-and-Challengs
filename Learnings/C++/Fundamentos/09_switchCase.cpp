42f600d91e96353f81327f482f80bec68636a684
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-07-19 22:58:03 -0300

    feat: switch case

---
+++ Learnings/C++/Fundamentos/09_switchCase.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "../Utils/utf8_config.h"
+using namespace std;
+
+int main() {
+  setupUTF8Console();
+
+  int diaDaSemana;
+  cout << endl << "Qual é o dia da semana? " << endl;
+  cin >> diaDaSemana;
+
+  switch (diaDaSemana) {
+    case 1:
+      cout << "Domingo";
+      break; // sai do switch
+    case 2:
+      cout << "Segunda-feira";
+      break;
+    case 3:
+      cout << "Terça-feira";
+      break;
+    case 4:
+      cout << "Quarta-feira";
+      break;
+    case 5:
+      cout << "Quinta-feira";
+      break;
+    case 6:
+      cout << "Sexta-feira";
+      break;
+    case 7:
+      cout << "Sábado";
+      break;
+    default:
+      cout << "Dia da semana inválido";
+  }
+
+  return 0;
+}
