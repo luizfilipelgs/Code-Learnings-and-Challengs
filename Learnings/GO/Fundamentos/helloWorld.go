d751dc19fccb8e063a1d7bc4dcb750fa4ca8cebc
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-03-19 10:57:33 -0300

    feat: hello world go

--- Learnings/GO/Fundamentos/helloWorld.go
+++ Learnings/GO/Fundamentos/helloWorld.go
@@ -1,8 +1,11 @@
-/* package main
+package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	println("Hello, World!")
+
+    var a int = 10
+
+    fmt.Println("Go é",a)
 }
- */
